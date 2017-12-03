f956d741810c498136985e2c0397d68b4b135511
Thiago <thiagoeh@gmail.com>
2017-12-03 00:16:49 -0200

    And now day 1 and half

--- day1/aoc_day1_and_half.py
+++ day1/aoc_day1_and_half.py
@@ -6,21 +6,19 @@
 # the code will fail when repeating non numeric chars
 input_seq = input()
 
+input_len = len(input_seq)
 
-# append first algarism to the end of a new 'extended'
+# append the entire input to itself. We only need half of it ahead, but anyway...
 # that way we can use a single 'for' loop for everything
-input_seq_extended = str(input_seq) + str(input_seq[0])
-
+input_seq_extended = str(input_seq) + str(input_seq)
 
 # initialize the output as an integer
 total_output = 0
 
-for i in range(0, len(input_seq)):
-    # not sure if this checking of range happens every loop. 
-    # # maybe its inefficient
+for i in range(0, input_len):
     
     # permute all the input checking for repeated chars
-    if input_seq_extended[i] == input_seq_extended[i+1]:
+    if input_seq_extended[i] == input_seq_extended[int(i+(input_len/2))]:
         print("Match! "+ str(i) + " Total output: " + str(total_output))
         total_output += int(input_seq_extended[i])        
 
